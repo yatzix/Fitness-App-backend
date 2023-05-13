3c7e97b04c1c4584cd248d72a498796a93a8f4a7
Sung Kim <stwkim103@gmail.com>
2023-05-13 14:53:19 -0400

    configure mongoDB connection

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL);
+
+// Shortcut variable to the Connection Object
+const db = mongoose.connection;
+
+// Listener for Connected Events
+db.on('connected', function() {
+    console.log(`Connected to MongoDB ${db.name} on ${db.host} port: ${db.port}`);
+});
