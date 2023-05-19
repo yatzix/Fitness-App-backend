d2251cf83b73d1d357e83cb1389d3e1848971de8
dangabrielle <hoangabrielle@gmail.com>
2023-05-18 19:17:51 -1000

    testing ctrlr

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,12 +9,16 @@ const { raw } = require("body-parser");
 router.get("/test", (req, res) => {
   res.send("testing233e");
 });
+router.get("/test", (req, res) => {
+  res.send("testing233e");
+});
 
 // router.get("/", usersCtrl.login);
 // POST /api/users
 router.post("/", usersCtrl.create);
 // router.post("/data", usersCtrl.create);
 router.post("/login", usersCtrl.login);
+router.get("/login", usersCtrl.login);
 router.get("/check-token", usersCtrl.checkToken);
 
 // Insert ensureLoggedIn on all routes that need protecting
